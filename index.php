941caeb8deb8687308c2228876963191c250ba63
rctorr <rictor@cuhrt.com>
2015-10-19 06:12:36 -0500

    combinando todo

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title>Base</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/main.css"> 
+</head>
+    
+<body>
+    <header>
+        <h1>Menú</h1>
+        
+        
+        
+    </header>
+    
+    <section id="contenido">
+        <div id="inicio">
+            <h2> Inicio</h2>
+        
+        </div>
+            
+            
+        
+        
+    </section>
+
+<footer>
+    Píe
+</footer>
+    </body>
+</html>
